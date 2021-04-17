92c1f868aee7a8667af4b43007fc873f0ec591fd
Artem Mamichev <spark@MacBook-Pro-Artem.local>
2021-04-17 11:15:46 +0300

    card + parrot

--- client/src/components/homePage/Card/Card.js
+++ client/src/components/homePage/Card/Card.js
@@ -2,17 +2,48 @@ import React from "react";
 import { Card, TextField } from "@material-ui/core";
 import "./Card.css";
 
+import { makeStyles } from '@material-ui/core/styles';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import logo from './Parrot.gif';
+
+const MediaStyle = makeStyles({
+    root: {
+      maxWidth: 300,
+    },
+    media: {
+      height: 600,
+    },
+  });
+
 export default function MediaCard() {
-  
+    const classes = MediaStyle();
+
     return (
-        <Card className="card">
-            <TextField
-                fullWidth
-                id="input email"
-                label="Pet name"
-                variant="outlined"
-                type="text"
+        <Card className="card" variant="outlined">
+            <CardActionArea>
+            <CardMedia
+                className={classes.media}
+                image={logo}
             />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Пират
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                Че-то там
+                </Typography>
+            </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Кнопочка
+                </Button>
+            </CardActions>
         </Card>
     );
   }
