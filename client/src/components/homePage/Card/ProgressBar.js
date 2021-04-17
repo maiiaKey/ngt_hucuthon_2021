14ae01f327688fe8d4b2f582ba8f13fc2f36bb03
Artem Mamichev <spark@MacBook-Pro-Artem.local>
2021-04-17 15:51:49 +0300

    corrected progress bar

--- client/src/components/homePage/Card/ProgressBar.js
+++ client/src/components/homePage/Card/ProgressBar.js
@@ -1,24 +1,55 @@
-import React from "react"
-import "./ProgressBar.css"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-
-const Progress = ({done}) => {
-	const [style, setStyle] = React.useState({});
-	
-	return (
-		<div className="progress">
-			<div className="progress-done" style={style}>
-				{done}%
-			</div>
-		</div>
-	)
+function LinearProgressWithLabel(props) {
+  return (
+    <Box display="flex" alignItems="center">
+      <Box width="100%" mr={1}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box minWidth={35}>
+        <Typography variant="body2" color="textSecondary">{`${Math.round(
+          props.value,
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
 }
 
-export default function App(){
-	return (
-	<>
-		<h1>Progress Bar</h1>
-		<Progress done="60"/>
-	</>
-	)
-}	
+LinearProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
+export default function LinearWithValueLabel() {
+  const classes = useStyles();
+  const [progress, setProgress] = React.useState(10);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 10));
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <LinearProgressWithLabel value={progress} />
+    </div>
+  );
+}
