968816ba6d288cc380522336f607558b121daec0
Dmytro Kolisnyk <dimka.kolesnik@gmail.com>
2021-04-17 13:24:54 +0200

    Gets sum of all grades in a course for a given email of a student

--- api/controllers/marks.js
+++ api/controllers/marks.js
@@ -1,33 +1,27 @@
-//TODO: delete all usages of FileSystem - use DataBase instead
 const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
-
-//TODO: delete all usages of myConsole
-const myConsole = new console.Console(fs.createWriteStream('./logs/results.json'));
-//TODO: delete all usages of WORKFOLDER
-const WORKFOLDER = 'classroomapi/';
-//TODO: think about can this be configured in the database, for different platforms
-//TODO: create a Table with Platform authorizations: "Platform", "Url?", "Token", "Credentials?"
-const SCOPES = ['https://www.googleapis.com/auth/classroom.rosters https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.coursework.students.readonly https://www.googleapis.com/auth/classroom.student-submissions.me.readonly https://www.googleapis.com/auth/classroom.student-submissions.students.readonly'];
-
+let sum = 0;
+const WORKFOLDER = 'controllers/';
+const SCOPES = ['https://www.googleapis.com/auth/classroom.profile.emails https://www.googleapis.com/auth/classroom.rosters https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.coursework.students.readonly https://www.googleapis.com/auth/classroom.student-submissions.me.readonly https://www.googleapis.com/auth/classroom.student-submissions.students.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+function authorize(email, credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-
-    // TODO: Check in Database
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
         if (err) return getNewToken(oAuth2Client, callback);
         oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+        callback(oAuth2Client, email);
     });
 }
 
@@ -52,8 +46,6 @@ function getNewToken(oAuth2Client, callback) {
         oAuth2Client.getToken(code, (err, token) => {
             if (err) return console.error('Error retrieving access token', err);
             oAuth2Client.setCredentials(token);
-            // TODO: Store the token to DataBase
-            // Store the token to disk for later program executions
             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
                 if (err) return console.error(err);
                 console.log('Token stored to', TOKEN_PATH);
@@ -68,16 +60,15 @@ function getNewToken(oAuth2Client, callback) {
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listCourses(auth) {
+
+function listCourses(auth, studentEmail) {
     const classroom = google.classroom({ version: 'v1', auth });
     classroom.courses.list({
     }, (err, res) => {
         if (err) return console.error('The API returned an error: ' + err);
         const courses = res.data.courses;
         if (courses && courses.length) {
-            courses.forEach((course) => {
-                course_id = course.id;
-            });
+            const course_id = courses[0].id;
             grades = [];
             user_names = [];
             assignment_names = [];
@@ -86,11 +77,13 @@ function listCourses(auth) {
                 res.data.studentSubmissions.forEach((submission) => { if (submission.assignedGrade) { grades.push({ "assignmentId": submission.courseWorkId, "userId": submission.userId, "grade": submission.assignedGrade }) } });
                 grades.sort((a, b) => b.assignmentId - a.assignmentId || b.userId - a.userId);
                 classroom.courses.students.list({ "courseId": course_id }, (err, res) => {
-                    res.data.students.forEach((user) => { user_names.push({ "userId": user.userId, "fullName": user.profile.name.fullName }) });
+                    res.data.students.forEach((user) => { user_names.push({ "userId": user.userId, "emailAddress": user.profile.emailAddress }) });
                     classroom.courses.courseWork.list({ "courseId": course_id }, (err, res) => {
                         res.data.courseWork.forEach((task) => { assignment_names.push({ "assignmentId": task.id, "title": task.title }) });
-                        grades.forEach((el) => { extract.push({"assignmentName": assignment_names.find(element => element["assignmentId"] == el.assignmentId).title , "studentName": user_names.find(element => element["userId"] == el.userId).fullName , "studentGrade": el.grade}) });
-                        myConsole.log(JSON.stringify(extract));
+                        grades.forEach((el) => { extract.push({ "assignmentName": assignment_names.find(element => element["assignmentId"] == el.assignmentId).title, "emailAddress": user_names.find(element => element["userId"] == el.userId).emailAddress, "studentGrade": el.grade }) });
+                        extract.forEach((combo) => {
+                            if (studentEmail == combo.emailAddress) { sum += combo.studentGrade }
+                        });
                     });
                 });
             });
@@ -100,31 +93,28 @@ function listCourses(auth) {
     });
 }
 
-
-// TODO: will accept user id and get user email
-// TODO: will return a total number of marks for this user for all courses
 const handleGetTotalMarks = (db) => (req, res) => {
     const { id } = req.params;
     db.select('Email').from('Users').where('Id', id)
-    .then(email => {
-        if (email) {
-            res.json(email)
-            //TODO: call Google api for user with this Email
-        }
-        else {
-            res.status(400).json('User with such id not found')
-        }
-    })
-    .catch(err => res.status(400).json('error getting user'))
-
-    // TODO: load client credentials from the database
-    // Load client secrets from a local file.
-    // fs.readFile(WORKFOLDER+'credentials.json', (err, content) => {
-    //     if (err) return console.log('Error loading client secret file:', err);
-    //     // Authorize a client with credentials, then call the Google Classroom API.
-    //     authorize(JSON.parse(content), listCourses);
-    // });
+        .then(email => {
+            if (email) {
+                email = email[0].Email;
+                sum = 0;
+                fs.readFile(WORKFOLDER + 'credentials.json', (err, content) => {
+                    if (err) return console.log('Error loading client secret file:', err);
+                    // Authorize a client with credentials, then call the Google Classroom API.
+                    return authorize(email, JSON.parse(content), listCourses);
+                })
+                setTimeout(function () {
+                    res.json(sum)
+                }, 4000)
+            }
+            else {
+                res.status(400).json('User with such id not found')
+            }
+        })
+        .catch(err => res.status(400).json("error when trying to find user by email"))
 
 }
 
-module.exports = { handleGetTotalMarks} 
+module.exports = { handleGetTotalMarks }
